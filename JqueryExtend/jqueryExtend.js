7b8868bf1a7d7d53827099f829172bbf6d0c7e57
huoxudong125 <423huo@163.com>
2017-11-17 16:10:35 +0800

    Add some extend function for jquery

--- JqueryExtend/jqueryExtend.js
+++ JqueryExtend/jqueryExtend.js
@@ -1,19 +1,22 @@
 jQuery.fn.extend({
 
+    //we using val() directly is enough
     getSelectedValue: function () {
-    //    var result= this.find("option:selected").val();
-       var result= this.val();
-       return result;
+        //    var result= this.find("option:selected").val();
+        var result = this.val();
+        return result;
     },
 
     getSelectedText: function () {
-       var result= this.find("option:selected").text();
-       return result;
-    }
+        var result = this.find("option:selected").text();
+        return result;
+    },
 
+    triggerChange: function () {
+        this.trigger("change");
+    },
 
+    triggerClick: function () {
+        this.trigger("click");
+    },
 });
-
-
-
-
